2a0d1967b5c72b5a62b9581e628612ce8beec6cd
antoine <antoine.drionduchapoi@student.vinci.be>
2024-01-14 13:50:26 +0100

    berlinClock refactor

--- __tests__/berlinClock.test.js
+++ __tests__/berlinClock.test.js
@@ -1,6 +1,18 @@
 // berlinClock.test.js
 const { berlinClock } = require('../berlinClock');
 
-test('Add minutes', () => {
+test('Add minutes 1', () => {
     expect(berlinClock(1)).toEqual([1, 0, 0, 0]);
+});
+
+test('Add minutes 2', () => {
+    expect(berlinClock(2)).toEqual([0, 1, 0, 0]);
+});
+
+test('Add minutes 3', () => {
+    expect(berlinClock(3)).toEqual([0, 0, 1, 0]);
+});
+
+test('Add minutes 4', () => {
+    expect(berlinClock(4)).toEqual([0, 0, 0, 1]);
 });
--- berlinClock.js
+++ berlinClock.js
@@ -1,7 +1,16 @@
 // berlinClock.js
 function berlinClock(minutes) {
-    console.log(minutes);
-    let count = [1, 0, 0, 0];
+    let count = [0,0,0,0];
+    if(minutes === 1){
+         count = [1, 0, 0, 0];
+    }else if (minutes === 2){
+         count = [0, 1, 0, 0];
+    }else if (minutes === 3){
+         count = [0, 0, 1, 0];
+    }else{
+         count = [0, 0, 0, 1];
+    }
+    
     return count;
 }
 
